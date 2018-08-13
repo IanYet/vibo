307ee516f853380c1d1d2abd7ebaaff54545b500
Ian <yt2015.97@gmail.com>
2018-08-14 01:45:04 +0800

    bug fixed

--- source/lib/diff.js
+++ source/lib/diff.js
@@ -1,10 +1,9 @@
 const fs = require('fs')
 const crypto = require('crypto')
-const path = require('path')
 
 const { DOCS_DIR, DB_DIR } = require('./constant')
 const filesHash = require('../db/hash.json')
-const { getFilePromise, setFilePromise } = require('../lib/utils')
+const { setFilePromise } = require('../lib/utils')
 
 const walkPromise = new Promise((resolve, reject) => {
     fs.readdir(DOCS_DIR, (err, files) => {
@@ -19,21 +18,18 @@ const walkPromise = new Promise((resolve, reject) => {
 module.exports =  walkPromise.then((files) => new Promise((resolve, reject) => {
     const changedFiles = []
 
-    files.forEach((file, index) => {
-        getFilePromise(DOCS_DIR + file).then((fileContent) => {
-            const fileHash = crypto.createHash('md5').update(fileContent).digest('hex')
+    files.forEach((file) => {
+        const fileContent = fs.readFileSync(DOCS_DIR + file)
+        const fileHash = crypto.createHash('md5').update(fileContent).digest('hex')
 
-            if (fileHash !== filesHash[file]) {
-                changedFiles.push(file)
-                filesHash[file] = fileHash
-            }
-
-            if (index === files.length - 1) resolve(changedFiles)
-        })
+        if (!filesHash[file] || fileHash !== filesHash[file]) {
+            changedFiles.push(file)
+            filesHash[file] = fileHash
+        }
     })
-})).then((changedFiles) => {
-    //to fix bugs
-    setFilePromise(`${DB_DIR}hash.json`, JSON.stringify(filesHash)).then(() => console.log(changedFiles))
 
-    return changedFiles
-})
+    resolve(changedFiles)
+
+})).then((changedFiles) => setFilePromise(`${DB_DIR}hash.json`, JSON.stringify(filesHash))
+    .then(() => changedFiles))
+    .catch((err) => console.log(err))
